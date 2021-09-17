b635ef46fcb325f8e99c3d3b4844e37ba1abf588
JoshKolles24 <824787@mystma.org>
2021-09-17 14:48:35 -0500

    Made a Mad Lib

--- main.py
+++ main.py
@@ -6,46 +6,23 @@ def topTwoGames():
     #print second game
     print("Super Smash Bros")
 
-topTwoGames() 
-
-def favoriteCat():
+def plusFive(num):
+    print(num + 5)
     
-    #print favorite cat
-    print("Odie")
+plusFive(10)
 
-favoriteCat()
+def divideTwo(num):
+    print(num / 2)
 
-def foodsCanned():
+divideTwo(4)
 
-    #print pickels
-    print("pickles")
+def addTwoNumbas(x, y):
+    print(x + y)
 
-    #print salsa
-    print("salsa")
+addTwoNumbas(11, 40)
 
-foodsCanned()
+def madLib(q, w, e, r, t, y, u, i, o, p, a, s, d, f, g, h, j, k):    
+    
+    print("While I've stayed at home, I've practiced the" + q +"for" + w +"hours every" + e +". My favoite key signature to play and practice in is b" + r + t +". It has"+ y +"flats and" + u +"sharps. I like this key signaure because it is" + i +"and" + o +". I've also gotten awesome at counting time signatures. The time signature I love is" + p +", because it is" + a +"to count. One instrument I want to learn how to playwhile I'm at home is" + s +", because it is" + d +"and sounds like a" + f +". My" + g +"likes it when I play" + h +", and always gives me a round of applause after my performances! When I reture to school, my teacher will be" + j +"how great I am playing my instrument. My teacher might even think I sound like" + k +"!")
 
-def leastFavoriteFoods():
-
-    #print yogurt
-    print("yogurt")
-
-    #print peaches
-    print("peaches")
-
-leastFavoriteFoods()
-
-def randomWord():
-
-    #print random word
-    print("chair")
-
-randomWord()
-
-print("22")
-
-def myNameIs(odie):
-    print(odie + " is my name")
-
-myNameIs("Josh")
-myNameIs("Collin")
+madLib(" Saxophone ", " 24 ", " Friday ", " z ", " minor ", " 22 ", " 22 ", " fuzzy ", " tasty ", " 16/16 ", " crazy ", " trumpet ", " chilly ", " bear ", " cat ", " mozart ", " worried ", " Hiroyuki ")
